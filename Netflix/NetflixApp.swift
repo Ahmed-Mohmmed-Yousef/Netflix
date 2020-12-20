5ac38bf570a6a56c22d7e69c49ad4703aaa86dd8
Ahmed Mohamed Yousef <a41155098@gmail.com>
2020-12-20 07:36:17 +0200

    first commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Ahmed on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
